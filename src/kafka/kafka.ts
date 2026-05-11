dc7220a96bd935d392096cdf43f39c3f6f142ce5
mildoss <vlad.maslo.05@gmail.com>
2026-05-11 14:37:00 +0300

    (feat): implement Kafka producer for payment success events

---
+++ src/kafka/kafka.ts
@@ -0,0 +1,59 @@
+import { Kafka, Producer } from 'kafkajs';
+import dotenv from 'dotenv';
+import fs from 'fs';
+import path from "node:path";
+
+dotenv.config();
+
+const certsPath = path.resolve(__dirname, '../../certs');
+
+const kafka = new Kafka({
+  clientId: process.env.KAFKA_CLIENT_ID,
+
+  brokers: [
+    process.env.KAFKA_BROKERS as string
+  ],
+
+  ssl: {
+    rejectUnauthorized: true,
+    ca: [fs.readFileSync(path.join(certsPath, 'ca.pem'), 'utf-8')],
+    key: fs.readFileSync(path.join(certsPath, 'service.key'), 'utf-8'),
+    cert: fs.readFileSync(path.join(certsPath, 'service.cert'), 'utf-8'),
+  },
+});
+
+const producer: Producer = kafka.producer();
+
+export const connectKafka = async () => {
+  try {
+    await producer.connect();
+  } catch {
+    process.exit(1);
+  }
+};
+
+export const sendPaymentSuccessEvent = async (paymentData: {
+  userId: string;
+  creditsToAdd: number;
+  amountPaid: number;
+  stripeSessionId: string;
+}) => {
+  try {
+    await producer.send({
+      topic: 'billing-payments-success',
+      messages: [
+        {
+          key: paymentData.userId,
+          value: JSON.stringify({
+            eventType: 'PAYMENT_SUCCESS',
+            timestamp: new Date().toISOString(),
+            data: paymentData,
+          }),
+        },
+      ],
+    });
+
+  } catch (error) {
+    console.error('❌ Kafka send error:', error);
+  }
+};
